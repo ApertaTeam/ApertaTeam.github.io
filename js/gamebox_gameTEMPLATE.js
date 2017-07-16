f19f5c4523a8c475e2e4728ec8147b46b2d819ee
MadCreativity <ahlgreenmadsen@gmail.com>
2017-07-16 18:42:52 +0200

    Delete gamebox_gameTEMPLATE.js

--- js/gamebox_gameTEMPLATE.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-    /*LOAD CANVAS*/
-    var canvas = document.getElementsByClassName("gameCanvas")[0];
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.imageSmoothingEnabled = false;
-});
